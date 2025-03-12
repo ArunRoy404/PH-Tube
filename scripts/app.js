c9ccf65790b7184d1d5706de468c36b8d2eedd80
ArunRoy404 <arunroy0065@gmail.com>
2025-03-12 16:16:06 +0600

    search videos function added

--- scripts/app.js
+++ scripts/app.js
@@ -29,6 +29,12 @@ const getCategoryVideo = (cat) =>{
         .then(data => loadVideo(data.category))
 }
 
+const getSearchedVideo = (text) =>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${text}`)
+    .then(r => r.json())
+    .then(data => loadVideo(data.videos))
+}
+
 const loadVideo = (videos) => {
     if(videos.length == 0){
         const noVideoBanner = document.getElementById('no-video')
@@ -41,7 +47,6 @@ const loadVideo = (videos) => {
 
     const videoContainer = document.getElementById('video-container')
     videoContainer.innerHTML = ""
-    console.log(videos[0])
     videos.forEach(video => {
         videoContainer.innerHTML += `
         <div class="card bg-base-100 shadow-sm">
@@ -66,8 +71,8 @@ const loadVideo = (videos) => {
                 <!-- detail container  -->
                 <div>
                     <h1 class="text-[#171717] font-bold">${video.title}</h1>
-                    <h2 class="flex gap-2 text-[#171717B3] text-sm">${video.authors[0].profile_name} 
-                    ${(video.authors[0].verified) ?`<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">`:``}</h2>
+                    <h2 class="flex gap-2 text-[#171717B3] text-sm">
+                        ${video.authors[0].profile_name} ${(video.authors[0].verified) ?`<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">`:``}</h2>
                     <p class="text-[#171717B3] text-sm">${video.others.views} views</p>
                 </div>
             </div>
@@ -96,6 +101,11 @@ const handleCatBtn = (cat) => {
     }
     selectActiveBtn(cat)
 }
+
+document.getElementById('search').addEventListener('keyup',(e)=>{
+    const searchText = e.target.value
+    getSearchedVideo(searchText)
+})
 
 getCategory()
 handleCatBtn("all")
